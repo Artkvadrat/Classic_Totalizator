ef1962e5babc0822ed6de98f3d586a9500ad6432
Danylo Nabok <nabok.danylo@gmail.com>
2021-03-19 10:29:29 +0200

    Project skeleton

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunkMiddleware from 'redux-thunk';
+
+const rootReducer = combineReducers({
+});
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+export default store;
