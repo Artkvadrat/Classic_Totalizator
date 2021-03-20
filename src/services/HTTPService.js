da6b1622001eaaf70a307e8f0f5f4d64c70a952d
Danylo Nabok <nabok.danylo@gmail.com>
2021-03-20 19:27:40 +0200

    Added HTTPService.js

--- src/services/HTTPService.js
+++ src/services/HTTPService.js
@@ -6,18 +6,14 @@ export default class HTTPService {
   static async request({ method = 'GET', path, body }) {
     const url = `${END_POINT}${path}`;
 
-    let jwtToken = '';
-
-    if (window.localStorage.length !== 0) {
-      jwtToken = window.localStorage.jwtToken;
-    }
+    const jwtToken = window.localStorage.getItem('jwtToken');
 
     const options = {
       method,
       headers: {
         Accept: 'text/plain',
         'Content-Type': 'application/json',
-        ...(jwtToken && { Authorization: jwtToken })
+        ...(!!jwtToken && { Authorization: jwtToken })
       },
       ...(body && { body: JSON.stringify(body) })
     };
