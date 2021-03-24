237026f51708eb6dd2475c075ddcc8a2d775e488
Danylo Nabok <nabok.danylo@gmail.com>
2021-03-24 12:29:47 +0200

    Resolved conflict

--- src/components/UsersPage/UsersPage.jsx
+++ src/components/UsersPage/UsersPage.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { loadUsers } from '../../ducks/usersPage/usersPage';
 
 const UserPage = () => {
   const dispatch = useDispatch();
 
-  const { isLoading, userList } = useSelector((state) => state.userPage);
+  // const { isLoading, userList } = useSelector((state) => state.userPage);
 
   useEffect(() => {
     dispatch(loadUsers());
