22cc1a210369aff43ee1e9b45ea48b1be53d0373
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-25 10:50:58 +0200

    Added Test for dateParse

---
+++ src/services/dateParse/dateParse.spec.js
@@ -0,0 +1,26 @@
+import parseDate from './dateParse';
+
+describe('parseDate test', () => {
+  it('should handle not valid input', () => {
+    expect(parseDate(null)).toEqual('');
+    expect(parseDate(0)).toEqual('');
+    expect(parseDate(42)).toEqual('');
+    expect(parseDate(-Infinity)).toEqual('');
+    expect(parseDate([])).toEqual('');
+    expect(parseDate(false)).toEqual('');
+    expect(parseDate(undefined)).toEqual('');
+    expect(parseDate('string')).toEqual('');
+    expect(parseDate(true)).toEqual('');
+    expect(parseDate({})).toEqual('');
+    expect(parseDate({ a: 'b' })).toEqual('');
+    expect(parseDate('2021-03-32')).toEqual('');
+    expect(parseDate('2021-11-2511:12:32.911Z')).toEqual('');
+  });
+  it('should provide correct format', () => {
+    expect(parseDate('2021-11-25T11:12:32.911Z')).toEqual('13:12 25.11.2021');
+    expect(parseDate('2018-10-24T12:12:32.911Z')).toEqual('15:12 24.10.2018');
+  });
+  it('should handle option when numbers less than 10', () => {
+    expect(parseDate('2021-03-03T03:03:03.911Z')).toEqual('05:03 03.03.2021');
+  });
+});
